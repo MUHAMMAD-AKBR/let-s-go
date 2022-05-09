05b5c948e2614074ba1488cf9e1b9bb07daa4965
akbar <akbarsmp27@gmail.com>
2022-05-09 17:07:05 +0700

    added a header to change from plain text to json.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/MUHAMMAD-AKBR/let-s-go/routes"
@@ -11,12 +12,18 @@ func main() {
 	// ------ STARTING THE SERVER ------//
 	// settings up the http handler using the router package
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/datas", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
+			w.Header().Set("Content-type", "application/json")
 			routes.GET(w, r)
+			fmt.Printf("path: %v", r.URL.Path)
 		case "POST":
 			routes.POST(w, r)
+		case "PUT", "PATCH":
+			fmt.Fprintf(w, "still working on it your request is PUT/PATCH")
+		case "DELETE":
+			fmt.Fprintln(w, "still working on it your request is DELETE")
 		}
 
 	})
